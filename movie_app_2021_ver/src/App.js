b1ddc30549b4fa85f7515997a885f2902170c105
SEOP <sshwang12345@gmail.com>
2021-07-28 04:03:12 +0900

    노마드코더 리액트 무비앱 만들기 연습♻️

--- movie_app_2021_ver/src/App.js
+++ movie_app_2021_ver/src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  render() {
-    return (
-      <div>
-        <h1>
-          The number is : {this.state.count}
-          <br />
-          <button onClick={this.add}>Add</button>
-          <button onClick={this.minus}>minus</button>
-        </h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
