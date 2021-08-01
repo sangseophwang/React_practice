3962edf95449f0f62735718ed8ec6d13ac601b60
SEOP <sshwang12345@gmail.com>
2021-08-02 02:58:11 +0900

    Seopflix 🎥

--- seopflix/src/Components/Router.js
+++ seopflix/src/Components/Router.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
 const Run = () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" component={TV} />
-    <Route path="/search" component={Search} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
 
