1c39c1c60939cd4992436a0005551ddd35e4fc5a
SEOP <sshwang12345@gmail.com>
2021-08-06 21:38:24 +0900

    Seopflix 🎥

--- seopflix/src/Components/Header.js
+++ seopflix/src/Components/Header.js
@@ -2,7 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+`;
 
 const List = styled.ul`
   display: flex;
