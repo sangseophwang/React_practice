aee48f2a7716de9a0707383d2abb266b4c902110
SEOP <sshwang12345@gmail.com>
2021-08-12 21:33:20 +0900

    Seopflix 🎥

--- seopflix/src/Components/GlobalStyles.js
+++ seopflix/src/Components/GlobalStyles.js
@@ -1,23 +1,28 @@
+// 이 곳은 전체 페이지에 적용할 공통 SC를 담아두는 곳이다. 
+
 import { createGlobalStyle } from "styled-components";
+
+// styled-reset은 SC를 이용해서 cssㄹ르 초기화시킨다. 0의 상태로 시작하도록 만든다.
 import reset from 'styled-reset'
 
 const globalStyles = createGlobalStyle`
 
-${reset};
-a{
-    text-decoration: none;
-    color: inherit;
-}
-*{
-    box-sizing: border-box;
-}
-body{
-    font-family:--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 12px;
-    background-color:rgba(20, 20, 20, 1);
-    color: white;
-    padding-top: 50px;
-}
+    ${reset};
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    body{
+        font-family:--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 12px;
+        background-color:rgba(20, 20, 20, 1);
+        color: white;
+        padding-top: 50px;
+    }
+
 `
 
 export default globalStyles;
--- seopflix/src/Components/Header.js
+++ seopflix/src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -24,32 +24,35 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align:center;
-  border-bottom: 5px solid ${props.current ? "red" : 'transparent'};
+  border-bottom: 5px solid ${props => (props.current ? "#3498db" : 'transparent')};
+  transition: border-bottom .5s ease-in-out
 `;
 
 const SLink = styled(Link)`
-
-height: 50px;
-display: flex;
-align-items: center;
-justify-content: center;
-
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const exportHeader = () => (
+
+const exportHeader = ({ location: { pathname } }) => (
   <Header>
     <List>
-      <Item current={true}>
+      <Item current={ pathname === '/' }>
         <SLink href="/">Movies</SLink>
       </Item>
-      <Item current={true}>
+      <Item current={ pathname === '/tv' }>
         <SLink href="/tv">TV</SLink>
       </Item>
-      <Item current={true}>
+      <Item current={ pathname === '/search' }>
         <SLink href="/search">Search</SLink>
       </Item>
     </List>
   </Header>
 );
 
-export default exportHeader;
+// withRouter는 다른 컴포넌트를 감싸는 컴포넌트이다.
+// 그리고 Router에 어떠한 정보를 전달한다.
+// export하는 건 다른 컴포넌트가 안에 있는 withRouter이다.
+export default withRouter(exportHeader);
