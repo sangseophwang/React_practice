3046745b44b6c2757654caae1871b3494fd6289f
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-09-01 23:22:59 +0900

    Seopflix 🎥

--- seopflix/src/Routes/TV/TVContainer.js
+++ seopflix/src/Routes/TV/TVContainer.js
@@ -2,24 +2,36 @@ import React from "react";
 import TVPresenter from "./TVPresenter";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default class extends React.Component{
-  state= {
-    topRated:    null,
-    popular:     null,
+export default class extends React.Component {
+  state = {
+    topRated: null,
+    popular: null,
     airingToday: null,
-    loading:     true,
-    error:       null
+    loading: true,
+    error: null,
+  };
+
+  async componentDidMount() {
+    try {
+    } catch {
+      this.setState({
+        error: "Can't find movies information.",
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
   }
 
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
-    return( 
-    <TVPresenter 
-      topRated    = { topRated }
-      popular     = { popular } 
-      airingToday = { airingToday } 
-      loading     = { loading } 
-      error       = { error }
-    />
-  )}
+    return (
+      <TVPresenter
+        topRated={topRated}
+        popular={popular}
+        airingToday={airingToday}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
 }
