cfdec6969789246e1674663348a634c801e95805
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-09-03 23:32:12 +0900

    Seopflix 🎥

--- seopflix/src/Routes/Search/SearchContainer.js
+++ seopflix/src/Routes/Search/SearchContainer.js
@@ -2,24 +2,43 @@ import React from "react";
 import SearchPresenter from "./SearchPresenter";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default class extends React.Component{
-  state= {
+export default class extends React.Component {
+  state = {
     movieResults: null,
-    tvResults:    null,
-    searchTerm:   "",
-    loading:      true,
-    error:        null
-  }
+    tvResults: null,
+    searchTerm: "",
+    loading: true,
+    error: null,
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm != "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const movieResults = await this.setState({ loading: true });
+    } catch {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
 
   render() {
     const { movieResults, tvResults, searchTerm, loading, error } = this.state;
-    return( 
-    <SearchPresenter 
-      movieResults= { movieResults }
-      tvResults   = { tvResults } 
-      searchTerm  = { searchTerm } 
-      loading     = { loading } 
-      error       = { error }
-    />
-  )}
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
 }
