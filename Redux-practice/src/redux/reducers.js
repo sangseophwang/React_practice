b550edb1c84cc5563047df388a10aec9fe95d7bc
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-09-26 17:49:21 +0900

    Redux Practice_store, action, reducer, subscribe, dispatch

---
+++ Redux-practice/src/redux/reducers.js
@@ -0,0 +1,18 @@
+import { ADD_TODO } from "./actions";
+
+// state
+// ['코딩', '점심 먹기'];
+
+const initialState = [];
+
+export function todoApp(previousState = initialState, action) {
+  // 초기값 설정 부분
+  // if (previousState === undefined) {
+  //   return [];
+  // }
+  if (action.type === ADD_TODO) {
+    return [...previousState, action.todo];
+  }
+
+  return previousState;
+}
