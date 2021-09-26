53020a24ac3fe40206c7f85f8ebe54668ba5ab5b
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-09-26 22:27:20 +0900

    Redux Practice

--- Redux-practice/src/index.js
+++ Redux-practice/src/index.js
@@ -6,16 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { addTodo } from "./redux/actions";
 
-const unsubscribe = store.subscribe(() => {
+store.subscribe(() => {
   console.log(store.getState());
 });
 
 // console.log(store);
-
-store.dispatch(addTodo("coding"));
-store.dispatch(addTodo("read book"));
-store.dispatch(addTodo("eat"));
-unsubscribe();
 
 ReactDOM.render(
   <React.StrictMode>
